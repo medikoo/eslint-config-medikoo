90f812f9941532299202594b40d8f67e8f2bd160
Mariusz Nowak <medyk@medikoo.com>
2017-05-17 13:46:54 +0200

    feat: turn off "newline-per-chained-call" rule
    We want to accept chains that fit one line.

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ module.exports = {
 		"max-statements": ["error", 20, { ignoreTopLevelFunctions: true }],
 		"multiline-ternary": "off",
 		"newline-before-return": "off",
+		"newline-per-chained-call": "off",
 		"no-continue": "off",
 		"no-empty": ["error", { allowEmptyCatch: true }],
 		"no-extra-parens": ["error", "all", { nestedBinaryExpressions: false }],
