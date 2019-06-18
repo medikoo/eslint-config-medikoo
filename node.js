20b6ef587944c02e402edd210d381a43bf52bc45
Mariusz Nowak <mariusz@medikoo.com>
2019-06-18 10:05:40 +0200

    feat: Node.js dedicated configuration

---
+++ node.js
@@ -0,0 +1,12 @@
+// Node.js v8+
+
+"use strict";
+
+const { join } = require("path");
+
+module.exports = {
+	extends: join(__dirname, "index.js"),
+	env: { node: true },
+	parserOptions: { ecmaFeatures: { experimentalObjectRestSpread: true } },
+	rules: { "prefer-object-spread": "error" }
+};
