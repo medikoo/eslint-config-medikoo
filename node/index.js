7ceb99f25f7479435c3f6ddb3cf08dec88689f4f
Mariusz Nowak <mariusz@medikoo.com>
2019-10-09 16:05:19 +0200

    refactor: Move nodejs config to directory
    To make room for other variants

--- node/index.js
+++ node/index.js
@@ -5,7 +5,7 @@
 const { join } = require("path");
 
 module.exports = {
-	extends: join(__dirname, "index.js"),
+	extends: join(__dirname, "../index.js"),
 	env: { node: true },
 	parserOptions: { ecmaFeatures: { experimentalObjectRestSpread: true } },
 	rules: { "prefer-object-spread": "error" }
