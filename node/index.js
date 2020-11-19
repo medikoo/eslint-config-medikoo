f1f911fed9a8a6a6a40d2ae5f989fee9a66bcc63
Mariusz Nowak <medyk@medikoo.com>
2020-11-19 13:09:47 +0100

    feat: Upgrade engines support
    BREAKING CHANGE:
    In default configuration drop support for non compliant ES2019 engines (Node.js versions below v10)

--- node/index.js
+++ node/index.js
@@ -4,8 +4,4 @@
 
 const { join } = require("path");
 
-module.exports = {
-	extends: join(__dirname, "../index.js"),
-	env: { node: true },
-	rules: { "prefer-named-capture-group": "error" }
-};
+module.exports = { extends: join(__dirname, "../index.js"), env: { node: true } };
